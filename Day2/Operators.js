867a4e78e95fe1ddf14826bb448ccf2b35a305bf
pratikmandi <pratikmandi0984@gmail.com>
2024-07-14 11:07:54 +0530

    Day 2: Operators

---
+++ Day2/Operators.js
@@ -0,0 +1,62 @@
+//-------------------Activity 1: Arithmatic Operations-----------------//
+let a=4;
+let b=2;
+
+//Task 1: Addition
+console.log(a+b);
+
+//Task 2: Subtraction
+console.log(a-b);
+
+//Task 3: Multiplication
+console.log(a*b);
+
+//Task 4: Division
+console.log(a/b);
+
+//Task 5: Remainder
+console.log(a%b);
+
+//-------------------Activity 2: Assignment Operators-----------------//
+//Task 6: '+='
+console.log(b+=1);
+
+//Task 7: '-='
+console.log(a-=1);
+
+//-------------------Activity 3: Comparison Operators-----------------//
+//Task 8: '> and <'
+console.log(a>b);
+console.log(a<b);
+
+// Task 9: '>= and <='
+console.log(a>=b);
+console.log(a<=b);
+
+// Task 10: '== and ==='
+let c=5;
+let d='5';
+console.log(c==d); //true
+console.log(c===d); //false
+
+//-------------------Activity 4: Logical Operators-----------------//
+//Task 11: '&&'
+if(c==5 && 2==2)
+    console.log("True Statement");
+else
+    console.log("False Statement");
+
+//Task 12: '||'
+if(c==5 || 3==2){
+    console.log("One condition is true.")
+}
+
+//Task 13: '!'
+let r=0;
+if(r !=425){
+    console.log("NOT 425");
+}
+
+//-------------------Activity 5: Logical Operators-----------------//
+//Task 14: 
+r>0 ? console.log(true):console.log(false);
